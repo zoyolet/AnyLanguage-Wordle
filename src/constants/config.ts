1f2751ea285e8f8e2e141690e65faae383978f0b
Kitchana Thomutok <kitchana.thamutok@slu.edu>
2022-04-01 07:57:12 -0500

    initial project with kreyol letter and change BG

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: '#VokabiLago #Kreyòl (Wordle for Haitian Creole)', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'YourNameHere', // Put your name here so people know who made this Wordle!
   authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
@@ -13,6 +13,6 @@ export const CONFIG = {
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'en', // the default interface language
-  availableLangs: ['en', 'es'], // the options available to the user for translation languages
+  defaultLang: 'ht', // the default interface language
+  availableLangs: ['en', 'ht'], // the options available to the user for translation languages
 }
